b440a73eac2c8ce791de1776ff0e2b4aac2643f6
Brandon Walter <projects@brandonwalter.org>
2020-04-24 22:33:19 -0500

    Version Update

--- lib/feed/feed_screen.dart
+++ lib/feed/feed_screen.dart
@@ -85,7 +85,7 @@ class _FeedScreenState extends State<FeedScreen> {
               child: Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: Text('What\'s on your mind?',
-                    style: Theme.of(context).textTheme.bodyText2),
+                    style: Theme.of(context).textTheme.body2),
               ),
             ),
           ),
