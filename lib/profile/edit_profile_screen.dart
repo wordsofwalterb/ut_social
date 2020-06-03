07c8f474ea8d8036ab3c8966bcf26a08478c1712
codeofwalterb <brandon@parabeac.com>
2020-06-03 11:36:39 -0500

    Last updates

--- lib/profile/edit_profile_screen.dart
+++ lib/profile/edit_profile_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ut_social/core/blocs/user_bloc/user_bloc.dart';
 import 'package:ut_social/core/widgets/profile_avatar.dart';
 
@@ -14,12 +19,100 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
-  String coverPhotoUrl;
-  String avatarUrl;
+  UserAuthenticated userState;
+  File coverPhotoFile;
+  File avatarFile;
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _bioController.dispose();
+    super.dispose();
+  }
+
+  dynamic _showSelectImageDialog(File file) {
+    return Platform.isIOS ? _iosBottomSheet(file) : _androidDialog(file);
+  }
+
+  dynamic _iosBottomSheet(File file) {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoActionSheet(
+          title: const Text('Add Photo'),
+          actions: <Widget>[
+            CupertinoActionSheetAction(
+              onPressed: () => _handleImage(ImageSource.camera, file),
+              child: const Text('Take Photo'),
+            ),
+            CupertinoActionSheetAction(
+              onPressed: () => _handleImage(ImageSource.gallery, file),
+              child: const Text('Choose From Gallery'),
+            ),
+          ],
+          cancelButton: CupertinoActionSheetAction(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+        );
+      },
+    );
+  }
+
+  dynamic _androidDialog(File file) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Add Photo'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () => _handleImage(ImageSource.camera, file),
+              child: const Text('Take Photo'),
+            ),
+            SimpleDialogOption(
+              onPressed: () => _handleImage(ImageSource.gallery, file),
+              child: const Text('Choose From Gallery'),
+            ),
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Colors.redAccent,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _handleImage(ImageSource source, File file) async {
+    Navigator.pop(context);
+    FocusScope.of(context).unfocus();
+    File imageFile = await ImagePicker.pickImage(source: source);
+    if (imageFile != null) {
+      imageFile = await _cropImage(imageFile);
+      setState(() {
+        avatarFile = imageFile;
+      });
+    }
+  }
+
+  Future<File> _cropImage(File imageFile) async {
+    final File croppedImage = await ImageCropper.cropImage(
+      sourcePath: imageFile.path,
+      aspectRatio: const CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
+    );
+    return croppedImage;
+  }
 
   @override
   Widget build(BuildContext context) {
-    final userState = BlocProvider.of<UserBloc>(context);
+    userState = BlocProvider.of<UserBloc>(context).state as UserAuthenticated;
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -72,10 +165,13 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
           Positioned(
             bottom: -42,
             left: 15,
-            child: ProfileAvatar(
-              avatarUrl: '',
-              radius: 100,
-              size: 90,
+            child: GestureDetector(
+              onTap: () => _showSelectImageDialog(avatarFile),
+              child: ProfileAvatar(
+                avatarUrl: userState.currentUser.avatarUrl,
+                radius: 100,
+                size: 90,
+              ),
             ),
           ),
         ],
@@ -88,70 +184,166 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       SizedBox(
         height: 20,
       ),
-      TextFormField(
-        controller: _firstNameController,
-        decoration: InputDecoration(
-          alignLabelWithHint: true,
-          floatingLabelBehavior: FloatingLabelBehavior.never,
-          hintText: 'First Name',
-          // errorText: (state.error == 'Last name needs to be provided')
-          //     ? state.error
-          //     : null,
+      Container(
+        decoration: BoxDecoration(
+            border: Border(
+          top: BorderSide(
+            color: Color(0xff3d3d3d),
+          ),
+          bottom: BorderSide(
+            color: Color(0xff3d3d3d),
+          ),
+        )),
+        child: TextFormField(
+          style: TextStyle(
+            color: Color(0xfff1f1f1),
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          controller: _firstNameController,
+          decoration: InputDecoration(
+            filled: false,
+            hintStyle: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+            prefixIcon: Container(
+              padding: EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width * .35,
+              child: Text(
+                'First Name',
+                style: TextStyle(
+                  color: Color(0xfff1f1f1),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            alignLabelWithHint: true,
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+            hintText: userState.currentUser.firstName,
+            // errorText: (state.error == 'Last name needs to be provided')
+            //     ? state.error
+            //     : null,
+          ),
+          autovalidate: false,
+          autocorrect: false,
+          textCapitalization: TextCapitalization.words,
+          // focusNode: _lastNameFocus,
+          textInputAction: TextInputAction.done,
+          // onFieldSubmitted: (term) {
+          //    _fieldFocusChange(context, _lastNameFocus, _emailFocus);
+          // },
         ),
-        autovalidate: false,
-        autocorrect: false,
-        textCapitalization: TextCapitalization.words,
-        // focusNode: _lastNameFocus,
-        textInputAction: TextInputAction.next,
-        // onFieldSubmitted: (term) {
-        //    _fieldFocusChange(context, _lastNameFocus, _emailFocus);
-        // },
       ),
       SizedBox(
         height: 3,
       ),
-      TextFormField(
-        controller: _lastNameController,
-        decoration: InputDecoration(
-          alignLabelWithHint: true,
-          floatingLabelBehavior: FloatingLabelBehavior.never,
-          hintText: 'Last Name',
-          // errorText: (state.error == 'Last name needs to be provided')
-          //     ? state.error
-          //     : null,
+      Container(
+        decoration: BoxDecoration(
+            border: Border(
+          bottom: BorderSide(
+            color: Color(0xff3d3d3d),
+          ),
+        )),
+        child: TextFormField(
+          style: TextStyle(
+            color: Color(0xfff1f1f1),
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          controller: _lastNameController,
+          decoration: InputDecoration(
+            filled: false,
+            hintStyle: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+            prefixIcon: Container(
+              padding: EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width * .35,
+              child: Text(
+                'Last Name',
+                style: TextStyle(
+                  color: Color(0xfff1f1f1),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            alignLabelWithHint: true,
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+            hintText: userState.currentUser.lastName,
+            // errorText: (state.error == 'Last name needs to be provided')
+            //     ? state.error
+            //     : null,
+          ),
+          autovalidate: false,
+          autocorrect: false,
+          textCapitalization: TextCapitalization.words,
+          // focusNode: _lastNameFocus,
+          textInputAction: TextInputAction.done,
+          // onFieldSubmitted: (term) {
+          //    _fieldFocusChange(context, _lastNameFocus, _emailFocus);
+          // },
         ),
-        keyboardType: TextInputType.emailAddress,
-        autovalidate: false,
-        autocorrect: false,
-        textCapitalization: TextCapitalization.words,
-        // focusNode: _lastNameFocus,
-        textInputAction: TextInputAction.next,
-        // onFieldSubmitted: (term) {
-        //    _fieldFocusChange(context, _lastNameFocus, _emailFocus);
-        // },
       ),
       SizedBox(
         height: 3,
       ),
-      TextFormField(
-        controller: _bioController,
-        decoration: InputDecoration(
-          alignLabelWithHint: true,
-          floatingLabelBehavior: FloatingLabelBehavior.never,
-          hintText: 'Bio',
-          // errorText: (state.error == 'Last name needs to be provided')
-          //     ? state.error
-          //     : null,
+      Container(
+        decoration: BoxDecoration(
+            border: Border(
+          bottom: BorderSide(
+            color: Color(0xff3d3d3d),
+          ),
+        )),
+        child: TextFormField(
+          maxLines: 3,
+          style: TextStyle(
+            color: Color(0xfff1f1f1),
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          controller: _bioController,
+          decoration: InputDecoration(
+            filled: false,
+            hintStyle: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+            prefixIcon: Container(
+              padding: EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width * .35,
+              child: Text(
+                'Bio',
+                style: TextStyle(
+                  color: Color(0xfff1f1f1),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            alignLabelWithHint: true,
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+
+            hintText: userState.currentUser.bio,
+            // errorText: (state.error == 'Last name needs to be provided')
+            //     ? state.error
+            //     : null,
+          ),
+          autovalidate: false,
+          autocorrect: false,
+          textCapitalization: TextCapitalization.words,
+          // focusNode: _lastNameFocus,
+          textInputAction: TextInputAction.done,
+          // onFieldSubmitted: (term) {
+          //    _fieldFocusChange(context, _lastNameFocus, _emailFocus);
+          // },
         ),
-        keyboardType: TextInputType.emailAddress,
-        autovalidate: false,
-        autocorrect: false,
-        textCapitalization: TextCapitalization.words,
-        // focusNode: _lastNameFocus,
-        textInputAction: TextInputAction.next,
-        // onFieldSubmitted: (term) {
-        //    _fieldFocusChange(context, _lastNameFocus, _emailFocus);
-        // },
       ),
     ]);
   }
